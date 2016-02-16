287a9fb0185c3ce6c02d77e516a17417b30ccf8c
blackfoot24 <teaguegv@hotmail.com>
2016-02-16 14:48:38 -0700

    get specific bear

--- server.js
+++ server.js
@@ -50,6 +50,17 @@ router.route('/bears')
 	})
 });
 
+router.route('/bears/:bear_id')
+	.get(function(req, res){
+		Bear.findById(req.params.bear_id, function(err, bear){
+			if(err) {
+				console.log(err);
+			} else {
+				res.json(bear);
+			}
+		})
+	})
+
 
 app.use('/api', router);
 
